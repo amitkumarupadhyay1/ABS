98e8b838ad9b16bdf4887b1c5d46df9f52679fdc
Amit Kumar Upadhyay <aryan21211986@gmail.com>
2023-08-29 18:51:20 +0530

    Project Created

---
+++ ABShop/src/main/java/awadh/bakery/AbShopApplication.java
@@ -0,0 +1,13 @@
+package awadh.bakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbShopApplication.class, args);
+	}
+
+}
---
+++ ABShop/src/test/java/awadh/bakery/AbShopApplicationTests.java
@@ -0,0 +1,13 @@
+package awadh.bakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
