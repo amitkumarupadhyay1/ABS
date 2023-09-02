d5bff8bc95c4fbf63085efe297a012772434d46e
Amit Kumar Upadhyay <aryan21211986@gmail.com>
2023-09-02 18:32:23 +0530

    code update: Controller and Form added

--- ABShop/src/main/java/awadh/bakery/controllers/CustomerController.java
+++ ABShop/src/main/java/awadh/bakery/controllers/CustomerController.java
@@ -1,8 +1,28 @@
 package awadh.bakery.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import awadh.bakery.models.Customer;
+import awadh.bakery.models.CustomerRepository;
+
 
 @Controller
 public class CustomerController {
- 
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @GetMapping("/customer/form")
+    public String showCustomerForm(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "customer-form";
+    }
+
+    @PostMapping("/saveCustomer")
+    public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+    	customerRepository.save(customer);
+        return "redirect:/customer/form";
+    }
 }
--- ABShop/src/main/java/awadh/bakery/models/CustomerDAO.java
+++
@@ -1,6 +0,0 @@
-package awadh.bakery.models;
-
-public interface CustomerDAO {
-	Customer getCustomer(final int id);
-	void save(final Customer customer);
-}
