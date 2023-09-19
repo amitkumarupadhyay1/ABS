2d8c9af3203d9a5f42e0c434c542aa6d822fadb7
Amit Kumar Upadhyay <aryan21211986@gmail.com>
2023-09-19 17:32:45 +0530

    image upload to a folder

--- ABShop/src/main/java/awadh/bakery/controllers/CustomerController.java
+++ ABShop/src/main/java/awadh/bakery/controllers/CustomerController.java
@@ -1,11 +1,20 @@
 package awadh.bakery.controllers;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import awadh.bakery.models.Customer;
 import awadh.bakery.models.CustomerRepository;
 
@@ -15,6 +24,10 @@ public class CustomerController {
 
     @Autowired
     private CustomerRepository customerRepository;
+    @Value("${upload.directory}") // Get the upload directory path from application.properties
+    private String uploadDirectory;
+    
+    
     @GetMapping("/")
     public String redirectToCustomerList() {
         return "redirect:/customers/list";
@@ -34,32 +47,63 @@ public class CustomerController {
     }
 
     @PostMapping("/saveCustomer")
-    public String saveCustomer(@ModelAttribute("customer") Customer customer) {
-    	if (customer.getCustID()==0) {
-            
-            customerRepository.save(customer);
-        } else {
-            Customer existingCustomer = customerRepository.findById(customer.getCustID())
-                .orElseThrow(() -> new IllegalArgumentException("Invalid customer ID: " + customer.getCustID()));
-            existingCustomer.setName(customer.getName());
-            existingCustomer.setEmail(customer.getEmail());
-            existingCustomer.setPhone(customer.getPhone());
-            existingCustomer.setAddress(customer.getAddress());
-            customerRepository.save(existingCustomer);
+    public String saveCustomer(@ModelAttribute("customer") Customer customer,BindingResult bindingResult,
+            @RequestParam("imageFile") MultipartFile imageFile, RedirectAttributes redirectAttributes)throws IOException {
+    		
+    	if (bindingResult.hasErrors()) {
+            // Validation errors occurred, return to the form page
+            return "customer-form";
         }
+
+        // Validate the image size here
+        if (!imageFile.isEmpty() && imageFile.getSize() > 300 * 1024) {
+            bindingResult.rejectValue("imageFile", "image.size", "Image size exceeds the allowed limit");
+            return "customer-form";
+        }
+
+        // Save the image to the server
+        if (!imageFile.isEmpty()) {
+            String uploadDirectory = "src/main/resources/customerImages"; // Specify the storage location
+            String originalFileName = imageFile.getOriginalFilename();
+            String uniqueFileName = System.currentTimeMillis() + "_" + originalFileName;
+
+            try {
+                File file = new File(uploadDirectory, uniqueFileName);
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                fileOutputStream.write(imageFile.getBytes());
+                fileOutputStream.close();
+            } catch (IOException e) {
+                // Handle any IO exception
+                e.printStackTrace();
+                // You can return an error message or perform other error handling here
+            }
+            customer.setImageFileName(uniqueFileName);
+        }
+
+        // Save the customer data to the database
+        customerRepository.save(customer);
+
+        // Redirect to the customer list page
+        redirectAttributes.addFlashAttribute("successMessage", "Customer saved successfully");
         return "redirect:/customers/list";
+
     }
     
     @GetMapping("/customer/edit/{id}")
-    public String showEditCustomerForm(@PathVariable("id") int id, Model model) {
-        Customer customer = customerRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid customer ID: " + id));
+    public String showEditCustomerForm(@PathVariable("id") Long id, Model model) {
+        Customer customer = customerRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid customer ID: " + id));
         model.addAttribute("customer", customer);
-        return "customer-form";
+        return "update-form";
+    }
+    
+    @PostMapping("/customer/update")
+    public String UpdateCustomer(@ModelAttribute("customer") Customer customer) {
+    	customerRepository.save(customer);
+    	return "redirect:/customers/list";
     }
 
     @GetMapping("/customer/delete/{id}")
-    public String deleteCustomer(@PathVariable("id") int id) {
+    public String deleteCustomer(@PathVariable("id") Long id) {
         customerRepository.deleteById(id);
         return "redirect:/customers/list";
     }
--- ABShop/src/main/java/awadh/bakery/models/Customer.java
+++ ABShop/src/main/java/awadh/bakery/models/Customer.java
@@ -1,40 +1,93 @@
 package awadh.bakery.models;
 
+import java.io.Serializable;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 @Entity
 @Table(name="customer")
-public class Customer {
+public class Customer implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String getImageFileName() {
+		return imageFileName;
+	}
+
+	public void setImageFileName(String imageFileName) {
+		this.imageFileName = imageFileName;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) 
-	@Column(name="id",nullable=false)
-	private int custID;
+	@Column(nullable=false)
+	private Long id;
 	
-    public Customer(int custID, @NotEmpty(message = "Name cannot be empty") String name,
+    
+    public Customer() {}
+
+
+	@Column
+	@NotEmpty(message = "Name cannot be empty")
+    private String name;
+    
+	@Column
+    @Email(message = "Invalid email format")
+    private String email;
+    
+	@Column
+    @Pattern(regexp = "\\d{10}", message = "Invalid phone number format")
+    private String phone;
+	@Column
+	@NotEmpty(message = "Address cannot be empty")
+    private String address;
+	@Transient
+	@Size(max = 300 * 1024, message = "Image size must be less than or equal to 300 KB")
+	private MultipartFile imageFile;
+	
+	private String imageFileName; 
+
+	
+	public Customer(Long id, @NotEmpty(message = "Name cannot be empty") String name,
 			@Email(message = "Invalid email format") String email,
-			@Pattern(regexp = "\\d{10}", message = "Invalid phone number format") String phone, String address) {
+			@Pattern(regexp = "\\d{10}", message = "Invalid phone number format") String phone,
+			@NotEmpty(message = "Address cannot be empty") String address,
+			@NotEmpty(message = "Image is required") @Size(max = 307200, message = "Image size must be less than or equal to 300 KB") MultipartFile imageFile,
+			String imageFileName) {
 		super();
-		this.custID = custID;
+		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.address = address;
-	}
-    public Customer() {}
-
-	public int getCustID() {
-		return custID;
+		this.imageFile = imageFile;
+		this.imageFileName = imageFileName;
 	}
 
-	public void setCustID(int custID) {
-		this.custID = custID;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -69,26 +122,13 @@ public class Customer {
 		this.address = address;
 	}
 
-	@Column
-	@NotEmpty(message = "Name cannot be empty")
-    private String name;
-    
-	@Column
-    @Email(message = "Invalid email format")
-    private String email;
-    
-	@Column
-    @Pattern(regexp = "\\d{10}", message = "Invalid phone number format")
-    private String phone;
-	@Column
-	@NotEmpty(message = "Address cannot be empty")
-    private String address;
-
-	@Override
-	public String toString() {
-		return "Customer [custID=" + custID + ", name=" + name + ", email=" + email + ", phone=" + phone + ", address="
-				+ address + "]";
+	public MultipartFile getImageFile() {
+		return imageFile;
 	}
 
-    // Getters and setters
+	public void setImageFile(MultipartFile imageFile) {
+		this.imageFile = imageFile;
+	} 
+	
+
 }
